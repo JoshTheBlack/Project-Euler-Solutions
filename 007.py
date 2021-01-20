fa8f1edefd59b4b478534c5ff469f1f6a75c38dd
joshtheblack <joshtheblack@gmail.com>
2021-01-20 17:39:08 -0500

    Speedier calculation

--- 007.py
+++ 007.py
@@ -8,9 +8,13 @@ import time
 def is_prime(n): 
     if n <= 1: 
         return False
+    if n == 2: 
+        return True
+    if n > 2 and n % 2 == 0: 
+        return False
   
     max_div = math.floor(math.sqrt(n)) 
-    for i in range(2, 1 + max_div): 
+    for i in range(3, 1 + max_div, 2): 
         if n % i == 0: 
             return False
     return True
