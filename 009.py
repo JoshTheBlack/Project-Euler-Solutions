4d7e1a6f31cb4834da18c7705fe05ae89666da24
JoshTheBlack <joshtheblack@gmail.com>
2021-01-21 13:40:01 -0500

    Error correction

--- 009.py
+++ 009.py
@@ -6,8 +6,8 @@
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-import time;
-from math import sqrt;
+import time
+from math import sqrt
 from sys import exit
 
 t0 = time.time()
@@ -17,7 +17,7 @@ for a in range(1,500):
         cSq = pow(a,2) + pow(b,2)
         c = sqrt(cSq)
         if c.is_integer() and a+b+int(c) == 1000:
-            print(a,b,int(c))
+            print(a*b*int(c))
             t1 = time.time()
             print(f"This calculation took {t1-t0} seconds")
             exit(0)
