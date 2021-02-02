20d7cfd318a2a85d1eeaf9ae577b0a903735af6d
JoshTheBlack <joshtheblack@gmail.com>
2021-02-02 14:51:34 -0500

    Cleaning up

--- Completed/020.py
+++ Completed/020.py
@@ -1,34 +1,13 @@
 # coding=utf-8
 # Find the sum of the digits in the number 100!
-from time import perf_counter
-from functools import wraps
-
-def timed(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = perf_counter()
-        result = func(*args, **kwargs)
-        end = perf_counter()
-        print(f"Function {func.__name__}: completed in {end-start:.6f} seconds")
-        return result
-    return wrapper
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-def sumDigits(n):
-    n = str(n)
-    x = 0
-    for number in n:
-        x += int(number)
-    return x
+import comm as comm
+from comm import timed
+#from functools import wraps
 
 @timed
 def driver(n):
-    x = factorial(n)
-    y = sumDigits(x)
+    x = comm.factorial(n)
+    y = comm.sumDigits(x)
     return y
 
 print(driver(100))
