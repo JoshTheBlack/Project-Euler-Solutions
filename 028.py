e58a6bc0fecc26b67db9a5b51a1e7b44ded5236b
JoshTheBlack <JoshTheBlack@gmail.com>
2021-02-14 12:23:44 -0500

    Cleaned up and organized code better.

--- 028.py
+++ 028.py
@@ -9,59 +9,56 @@
 # What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 from comm import timed
 
-def moveRight(x,y):
-    return x+1, y
+def moveRight(x,y, grid, count):
+    grid[y][x+1] = count
+    return x+1, y, grid, count+1
 
-def moveLeft(x,y):
-    return x-1, y
+def moveLeft(x,y, grid, count):
+    grid[y][x-1] = count
+    return x-1, y, grid, count+1
 
-def moveUp(x,y):
-    return x, y-1
+def moveUp(x,y, grid, count):
+    grid[y-1][x] = count
+    return x, y-1, grid, count+1
 
-def moveDown(x,y):
-    return x, y+1
+def moveDown(x,y, grid, count):
+    grid[y+1][x] = count
+    return x, y+1, grid, count+1
+
+def even(x, y, grid, count, n):
+    x, y, grid, count = moveLeft(x,y,grid,count)
+    for j in range(1,n+1):
+        x,y, grid, count = moveUp(x,y,grid,count)
+    for j in range(1,n+1):
+        x,y, grid, count = moveRight( x,y, grid, count)
+    return x, y, grid, count, n+1
+
+def odd(x, y, grid, count, n):
+    x, y, grid, count = moveRight(x,y,grid,count)
+    for j in range(1,n+1):
+        x,y, grid, count = moveDown(x,y,grid,count)
+    for j in range(1,n+1):
+        x,y, grid, count = moveLeft( x,y, grid, count)
+    return x, y, grid, count, n+1
+
 
 def createSpiralGrid(sz):
     grid = [[0 for i in range(sz)] for j in range(sz)]
-    n = 1
-    i = 2
-    x = sz // 2
-    y = x
+    n, count, x, y = 1, 2, sz // 2, sz // 2
     grid[y][x] = 1
-    while i <= sz**2:
+    while count <= sz**2:
         if n % 2 == 0:
-            x,y = moveLeft(x,y)
-            grid[y][x] = i
-            i += 1
-            for j in range(1,n+1):
-                x,y = moveUp(x,y)
-                grid[y][x] = i
-                i += 1
-            for j in range(1,n+1):
-                x,y = moveRight(x,y)
-                grid[y][x] = i
-                i += 1
-            n += 1
+            x, y, grid, count, n = even(x, y, grid, count, n)
         else:
-            x,y = moveRight(x,y)
-            grid[y][x] = i
-            i += 1
-            for j in range(1,n+1):
-                x,y = moveDown(x,y)
-                grid[y][x] = i
-                i += 1
-            for j in range(1,n+1):
-                x,y = moveLeft(x,y)
-                grid[y][x] = i
-                i += 1
-            n += 1
+            x, y, grid, count, n = odd(x, y, grid, count, n)
     return grid
 
 @timed
-def sumGridDiagonals(grid):
+def sumGridDiagonals(sz):
+    grid = createSpiralGrid(sz)
     sum = 0
     for i in range(len(grid)):
         sum += grid[i][i] + grid[len(grid)-1-i][i]
     return sum - grid[len(grid)//2][len(grid)//2]
 
-print(sumGridDiagonals(createSpiralGrid(1001)))
+print(sumGridDiagonals(1001))
