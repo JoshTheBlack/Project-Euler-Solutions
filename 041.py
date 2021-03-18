987910845096173e809a872a447b8e969e1791d1
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-18 17:48:46 -0400

    Instead of calculating all primes and checking for pandigitalness, create all pandigitals and check for primality.  Runs in under 1 second.  Approximately twice as fast as previous method.

--- 041.py
+++ 041.py
@@ -4,11 +4,21 @@
 # What is the largest n-digit pandigital prime that exists?
 from comm import timed
 import comm
+import itertools
+
+def generatePandigitalNumbers(max=9):
+    pandigitals = []
+    digits = [1]
+    for i in range(2,max+1):
+        digits.append(i)
+        for combination in itertools.permutations(digits):
+            pandigitals.append(''.join(map(str, combination)))
+    return pandigitals
 
 
 @timed
 def p41():
-    # Generate a prime sieve of all primes below 100 million (Guessing at upper bound)
+    # Generate a prime sieve of all primes below 7,654,321 (Guessing at upper bound)
     primes = comm.primeSieveRange(7654321)
     # Check each prime number to see if it is pandigital, if so save it into a list called pdprimes.
     pdprimes = []
@@ -20,4 +30,16 @@ def p41():
     # Return the largest pandigital prime.
     return pdprimes[0]
 
-print(p41())
+@timed
+def p41_2():
+    # generate all pandigital numbers of length n where n < 9 and store it in a list ordered smallest to largest named pandigitals
+    pandigitals = generatePandigitalNumbers(9) 
+    # check each number in pandigitals for primality.  If prime, store it in result.
+    for number in pandigitals:
+        if comm.is_prime(int(number)):
+            result = number
+    return result
+
+
+#print(p41())
+print(p41_2())
