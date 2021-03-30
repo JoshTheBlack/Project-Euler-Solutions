ec567adafed733327f82447afc96786935e757ad
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-30 17:52:34 -0400

    Project 47 beginning

--- 047.py
+++ 047.py
@@ -1,3 +1,36 @@
 # coding=utf-8
-# 
-from comm import timed
+# The first two consecutive numbers to have two distinct prime factors are:
+# 14 = 2 × 7 
+# 15 = 3 × 5
+
+# The first three consecutive numbers to have three distinct prime factors are:
+
+# 644 = 2² × 7 × 23
+# 645 = 3 × 5 × 43
+# 646 = 2 × 17 × 19.
+
+# Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+# %% 
+from comm import *
+from itertools import combinations
+from numpy import product
+
+@timed
+def p47():
+    primes = primeSieveRange(150)
+    test = combinations(primes,4)
+    result = []
+    for comb in test:
+        x = product(comb)
+        if x in result: continue
+        result.append(product(comb))
+    result = sorted(result)
+    for number in result:
+        if number + 1 not in result: continue
+        if number + 2 not in result: continue
+        if number + 3 not in result: continue
+        return number
+
+print(p47())
+
+# %%
