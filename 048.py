98aa1f8aff046ce07814a257cd8cfb25638c64b1
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-31 12:58:20 -0400

    Project 48 in under 1s

---
+++ 048.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+# Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000
+from comm import *
+
+@timed
+def p48():
+    x = 0
+    for y in range(1,1001):
+        x += y**y
+    return str(x)[-10:]
+
+print(p48())
