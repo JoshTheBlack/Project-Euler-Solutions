455803cada4d5ee450567db68e91305cc546fa15
JoshTheBlack <JoshTheBlack@gmail.com>
2021-04-18 16:06:40 -0400

    Project 57 done with recursion.  Similar speeds.

--- 057.py
+++ 057.py
@@ -1,5 +1,8 @@
 # coding = utf-8
 from comm import *
+import sys
+
+sys.setrecursionlimit(3500)
 
 @timed
 def p57(num=3, den=2, count=0, max=1000, result=0):
@@ -10,4 +13,17 @@ def p57(num=3, den=2, count=0, max=1000, result=0):
             result += 1
         if count == max: return result
 
+def cf(num,den,count=0,max=1000,result=0):
+        num,den = num+2*den, num+den
+        count += 1
+        if int(math.log10(num)) > int(math.log10(den)): result += 1
+        if count >= max: 
+            return result
+        return cf(num,den,count,max,result)
+
+@timed
+def p57_2(max=1000):
+    result = cf(3,2,max=max)
+
 print(p57())
+print(p57_2())
