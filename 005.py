68fb5ba42149371d75e7d5fe55b633e72d03fcc7
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-09 13:44:12 -0400

    Project 5 reimagined with timer

--- 005.py
+++ 005.py
@@ -1,21 +1,19 @@
 # coding=utf-8
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-#
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+from comm import timed, sumDigits
 
 def remainderFree(x):
-    isTrue = True
-    while isTrue == True:
-        for numbers in range(2,21,1):
-            if x % numbers != 0:
-                isTrue = False
-                break
-        return(isTrue)
+        for num in range(2,21,1):
+            if x % num != 0:
+                return False
+        return True
 
-stop = False
-y = 0
-while stop == False:
-    y += 20
-    stop = remainderFree(y)
+@timed
+def driver():
+    x = 20
+    while True:
+        x += 20
+        if remainderFree(x): return x
 
-print(y)
+print(driver())
