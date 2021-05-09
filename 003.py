2660de7aec41deb0621d7ecb958ff1563349245f
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-09 13:31:26 -0400

    Project 2 reimagined, with timer

--- 003.py
+++ 003.py
@@ -1,33 +1,15 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
-
 #What is the largest prime factor of the number 600851475143 ?
-from functools import reduce
+from comm import is_prime, timed, factors
 
-def find_factors(n):    
-    return sorted(set(reduce(list.__add__, 
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
+@timed
+def largestPrimeFactor(x = 600851475143):
+    facts = factors(x)
+    primefactors = []
+    for i in facts:
+        if is_prime(i):
+            primefactors.append(i)            
+    return max(primefactors)
 
-def is_prime(z):
-    n = 2
-    if z < n:
-        return False
-    else:    
-        while n < z:
-            if z % n == 0:
-                return False
-                break
-            n = n + 1
-        else:
-            return True
-
-x = 600851475143
-
-factors = find_factors(x)
-primefactors = []
-
-for i in factors:
-    prime = is_prime(i)
-    if prime == True:
-        primefactors.append(i)
-        
-print(max(primefactors))
+if __name__ == "__main__":
+    print(largestPrimeFactor())
