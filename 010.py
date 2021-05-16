a143214eac3d5db542eb1f213e7e2b73bf2a5401
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-16 14:29:33 -0400

    Refactored in to function, sped up 40x, and added more accurate timer.

--- 010.py
+++ 010.py
@@ -1,32 +1,12 @@
 # coding=utf-8
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
-import math 
-import time 
-def is_prime(n): 
-    if n <= 1: 
-        return False
-    if n == 2: 
-        return True
-    if n > 2 and n % 2 == 0: 
-        return False
-  
-    max_div = math.floor(math.sqrt(n)) 
-    for i in range(3, 1 + max_div, 2): 
-        if n % i == 0: 
-            return False
-    return True
-  
-# Driver function 
-t0 = time.time() 
-c = 0 #for counting 
-n = 0
-while n < 2000000:
-    n += 1
-    x = is_prime(n)
-    if x == 1:
-        c += n
+from comm import timed, primeSieveRange
 
-t1 = time.time() 
-print(f"{c} is the sum of all primes < 2 million")
-print("Time required :", t1 - t0) 
+@timed
+def p010(max = 2_000_000):
+    primes = primeSieveRange(max)
+    return sum(primes)
+
+if __name__ == "__main__":
+    print(p010())
