2ab92db7b310343b0ff6b2afcdb8f30b8b41c0c9
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-16 12:45:08 -0400

    Refactored in to function and added more accurate timer.

--- 009.py
+++ 009.py
@@ -1,23 +1,15 @@
 # coding=utf-8
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-#
-# a2 + b2 = c2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
-#
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-import time
-from math import sqrt
-from sys import exit
+from comm import timed
 
-t0 = time.time()
+@timed
+def p009():
+    for a in range(1,500):
+        for b in range(1,500):
+            c = (a**2 + b**2)**0.5
+            if c == int(c) and a + b + c == 1000:
+                return a * b * int(c)
 
-for a in range(1,500):
-    for b in range(1,500):
-        cSq = pow(a,2) + pow(b,2)
-        c = sqrt(cSq)
-        if c.is_integer() and a+b+int(c) == 1000:
-            print(a*b*int(c))
-            t1 = time.time()
-            print(f"This calculation took {t1-t0} seconds")
-            exit(0)
+if __name__ == "__main__":
+    print(p009())
