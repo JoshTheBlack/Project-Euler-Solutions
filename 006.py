bcd71f7528e4daf688e7fcae41285e952dd950cc
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-16 12:15:26 -0400

    Cleaned up, refactored in to functions, added timer.

--- 006.py
+++ 006.py
@@ -1,22 +1,26 @@
 # coding=utf-8
 # The sum of the squares of the first ten natural numbers is 385 (1sq + 2sq ... +10sq)
-#
 # The square of the sum of the first ten natural numbers is 3025 (1+2+3..+10)squared = 55sq = 3025
-#
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
-#
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-squareofsums = 0
-sumofsquares = 0
-numberRange = 100
+from comm import timed
 
-for num in range(1,numberRange + 1):
-    squareofsums += num
+def squareSum(arr):
+    result = 0
+    for num in arr:
+        result += num
+    return result ** 2
 
-squareofsums *= squareofsums
+def sumSquare(arr):
+    result = 0
+    for num in arr:
+        result += num ** 2
+    return result
+    
+@timed
+def p006(max = 100):
+    nums = [i for i in range(max+1)]
+    return squareSum(nums) - sumSquare(nums)
 
-for num in range(1,numberRange +1):
-    sumofsquares += num * num
-
-answer = squareofsums - sumofsquares
-print(answer)
+if __name__ == "__main__":
+    print(p006())
