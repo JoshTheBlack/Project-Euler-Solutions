b9cb7290e7770af38977ffd809f536b6f2bf1ea9
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-30 12:47:00 -0400

    Project 14 refactored in to functions and added more precise timer.

--- 014.py
+++ 014.py
@@ -12,30 +12,30 @@
 # Which starting number, under one million, produces the longest chain?
 # 
 # NOTE: Once the chain starts the terms are allowed to go above one million.
-import time
+from comm import timed
 
-def collatz(s):
+def collatz(s, ans):
     t = s
     count = 1
-    #num = [s]
     while s != 1:
         if s < t:
             count += ans[s-1][0] - 1
-            return count, t#, num
+            return count, t
         elif s % 2 == 0:
             s //= 2
             count += 1
         else:
             s = 3 * s + 1
             count += 1
-    return count, t #, num
+    return count, t
 
-t0 = time.time()
-ans = []
-for i in range(1, 1000000):
-    ans.append(collatz(i))
-ans.sort(reverse=True)
-print(f"The number {ans[0][1]} took {ans[0][0]} hops to 1.")
+@timed
+def p014():
+    ans = []
+    for i in range(1, 1000000):
+        ans.append(collatz(i, ans))
+    ans.sort(reverse=True)
+    return f"The number {ans[0][1]} took {ans[0][0]} hops to 1."
 
-t1 = time.time()
-print(f"This calculation took {t1-t0} seconds.") # Display time elapsed during calculation.
+if __name__ == "__main__":
+    print(p014())   
