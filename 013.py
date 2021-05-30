643085855fa9dc0d912bce2224a2e30fe856e393
JoshTheBlack <JoshTheBlack@gmail.com>
2021-05-30 12:43:09 -0400

    Project 13 refactored in to functions and added more precise timer.

--- 013.py
+++ 013.py
@@ -1,15 +1,20 @@
 # coding=utf-8
 # Work out the first ten digits of the sum of the following one-hundred 50-digit numbers. (in 13.txt)
-import os, time
+from comm import timed
+import os
 import numpy as np
 
-t0 = time.time()
-data = np.transpose(np.loadtxt(os.path.join(os.sys.path[0], "13.txt"), skiprows=0, delimiter=" "))
-total = 0
+def importData(fileName):
+    return np.transpose(np.loadtxt(os.path.join(os.sys.path[0], fileName), skiprows=0, delimiter=" "))
 
-for i in data:
-    total += int(i)
+@timed
+def p013(data):
+    total = 0
 
-print(str(total)[0:10])
-t1 = time.time()
-print(f"This calculation took {t1-t0} seconds.") # Display time elapsed during calculation.
+    for i in data:
+        total += int(i)
+
+    return str(total)[0:10]
+
+if __name__ == "__main__":
+    print(p013(importData("13.txt")))
