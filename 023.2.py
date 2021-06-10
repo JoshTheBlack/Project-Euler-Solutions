9596aed5ff0c938b62fbcbb807ca016128a153a8
JoshTheBlack <JoshTheBlack@gmail.com>
2021-06-10 11:47:37 -0400

    Refactored project 23.2

--- 023.2.py
+++ 023.2.py
@@ -1,11 +1,11 @@
 # coding=utf-8
-import comm
-from comm import timed
+from comm import timed, sumProperDivisors
 
 @timed
 def driver():
-    abundantSet = set(i for i in range(1,30000) if comm.sumProperDivisors(i) > i)
+    abundantSet = set(i for i in range(1,28124) if sumProperDivisors(i) > i)
     summedSet = set(x+y for x in abundantSet for y in abundantSet)
     return sum(set(range(1,28124)) - summedSet)
 
-print(driver())
+if __name__ == "__main__":
+    print(driver())
