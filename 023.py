9b08b0ede5c242b35b0ca67e371438eb012c7049
JoshTheBlack <JoshTheBlack@gmail.com>
2021-06-10 11:47:22 -0400

    Refactored Project 23

--- 023.py
+++ 023.py
@@ -7,14 +7,12 @@
 # However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as 
 # the sum of two abundant numbers is less than this limit.
 # Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-from comm import timed
-import comm
+from comm import timed, factors
 
-@timed
 def find_abundant_numbers():
     abundant = []
     for number in range(1,21823):
-        if sum(comm.factors(number),-number) > number:
+        if sum(factors(number),-number) > number:
             abundant.append(number)
     return abundant
 
@@ -35,4 +33,5 @@ def driver():
             answer.append(number)
     return sum(answer)
 
-print(driver())
+if __name__ == "__main__":
+    print(driver())
