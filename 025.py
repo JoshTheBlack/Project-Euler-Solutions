2448561fdd454103f0825e8139b87b8934ef3f94
JoshTheBlack <JoshTheBlack@gmail.com>
2021-06-10 11:48:10 -0400

    Refactored project 25

--- 025.py
+++ 025.py
@@ -3,7 +3,6 @@
 # Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
 # The 12th term, F12, is the first term to contain three digits.
 # What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
-import comm
 from comm import timed
 
 @timed
@@ -18,4 +17,5 @@ def fibbonacciLengthIndex(n):
         length = len(str(c))
     return count
 
-print(fibbonacciLengthIndex(1000))
+if __name__ == "__main__":
+    print(fibbonacciLengthIndex(1000))
