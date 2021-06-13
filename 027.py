d3528dcac6470ede04053037c8487ceeec220011
JoshTheBlack <JoshTheBlack@gmail.com>
2021-06-13 12:08:57 -0400

    Project 27 refactored

--- 027.py
+++ 027.py
@@ -1,16 +1,15 @@
 # coding=utf-8
 # Find the product of the coefficients, a and b, for the quadratic expression that produces
 # the maximum nuber of primes for consecutive values of n, starting with n = 0.
-from comm import timed
-import comm
+from comm import *
 
 def findNumberOfQuadraticPrimes(a,b):
-    if comm.is_prime(b):
+    if is_prime(b):
         count = 0
         stop = False
         while stop == False:
             for n in range(1000):
-                if comm.is_prime((n**2 + (a*n) + b)) == True:
+                if is_prime((n**2 + (a*n) + b)) == True:
                     count += 1
                 else:
                     stop == True
@@ -22,7 +21,7 @@ def findNumberOfQuadraticPrimes(a,b):
 def findLargestNumberOfQuadraticPrimes(n):
     results = {}
     results[0] = [0,0]
-    prime = comm.primes(n)
+    prime = primes(n)
     for b in prime:
         for a in range(-abs(b),n+1):
             number = findNumberOfQuadraticPrimes(a,b)
@@ -30,4 +29,5 @@ def findLargestNumberOfQuadraticPrimes(n):
                 results[number] = [a,b]
     return results[max(results)][0] * results[max(results)][1]
 
-print(findLargestNumberOfQuadraticPrimes(1000))
+if __name__ == "__main__":
+    print(findLargestNumberOfQuadraticPrimes(1000))
