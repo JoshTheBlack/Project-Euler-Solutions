4a64711f00cc07b4384d519cc100fcadeef36bf6
JoshTheBlack <JoshTheBlack@gmail.com>
2021-06-16 11:40:44 -0400

    Project 32 refactored

--- 032.py
+++ 032.py
@@ -4,11 +4,10 @@
 # The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
 # Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 # HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
-from comm import timed
-import comm
+from comm import timed, orderedFactors
 
 def isUnusual(number):
-    factors = comm.orderedFactors(number) # function to return a 2d array of factor pairs
+    factors = orderedFactors(number) # function to return a 2d array of factor pairs
     for i in range(0,len(factors)):
         string = "".join(sorted(str(number) + str(factors[i][0]) + str(factors[i][1]))) # create ordered string of factor pair and number
         if string == "123456789":
@@ -18,9 +17,10 @@ def isUnusual(number):
 @timed
 def sumUnusualProducts():
     answer = 0
-    for i in range(1000,10000):
+    for i in range(1_000,10_000):
         if isUnusual(i):
             answer += i
     return answer
 
-print(sumUnusualProducts())
+if __name__ == "__main__":
+    print(sumUnusualProducts())
